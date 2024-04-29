672f4053dadc3920e79632b01b897161c2107007
Diana_morales <dianacarolina.morales82@gmail.com>
2024-04-28 23:41:04 -0500

    ejercicio5

---
+++ ejercicio5/ejercicio5.js
@@ -0,0 +1,7 @@
+/*Escribe una función para calcular el área de un círculo. Puedes
+utilizar funciones declarativas.*/
+
+function areaCirculo(radio){
+    return Math.PI * Math.pow(radio, 2);
+}
+console.log(areaCirculo(5));
