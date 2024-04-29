3567f611ca20c7a0624afc856f9419ac56ae9ce6
Diana_morales <dianacarolina.morales82@gmail.com>
2024-04-28 23:30:27 -0500

    ejercicio3

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,7 +1,14 @@
 /* Crea una función que ordene un array de números de manera
 ascendente. Puedes utilizar funciones flecha.*/
-const ordenarArray = (array) =>
-array.sort((a, b) => a - b);
 
-console.log
+const sortArrayAscending = (arr) => arr.sort((a, b) => a - b);
+
+const numbers = [40, 1, 5, 200];
+console.log(sortArrayAscending(numbers)); // [1, 5, 40, 200]
+
+
+
+
+
+
 
